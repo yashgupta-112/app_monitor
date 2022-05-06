40fc38551f0a573ef3833c586db2fd4c8fe748be
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-05-06 23:42:55 +0530

    first commit

---
+++ App_status_check.py
@@ -0,0 +1,21 @@
+from ast import Pass
+import os
+import time
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+class app_monitor():
+    def rtorrent_monitor(self):
+        status = os.popen("pgrep rtorrent").read()
+        print(status)
+
+
+
+
+
+monitor = app_monitor()
+if __name__ == '__main__':
+    # s = input('Please enter name of application you want script to monitor(example rtorrent sonarr deluge):')
+    monitor.rtorrent_monitor()
