612fc58beec4ea903b89a7dcb7e13c2f83069815
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-05-11 18:15:14 +0530

    12th commit

--- App_status_check.py
+++ App_status_check.py
@@ -12,17 +12,54 @@ Variable for location of file below
 work_dir = os.getcwd()
 apps_file= '{}/bin/app_monitor/apps.txt'.format(work_dir)
 monitor_app_list = []
+rtorrent_log_file = '{}/bin/app_monitor/rtorrent.txt'.format(work_dir)
+docker_log_file = '{}/bin/app_monitor/rtorrent.txt'.format(work_dir)
+
 
 """
 Function are defined below
 """
 class app_monitor():
     def rtorrent_monitor(self):
-        status = os.popen("pgrep rtorrent").read()
-        if status == "":
-            os.system("app-rtorrent restart")
+        Pid = os.system('pgrep rtorrent')
+        Pid = int(Pid)
+
+        if Pid == 256: # Pid is 256 when os.system doesn't give O/P for Linux
+            os.system("app-rtorrent restart") #  restart app
+            with open(rtorrent_log_file, "a") as f:
+                f.write("\n\nTIME: "+current_time+"\n")
+                f.write('rTorrent was down and has been RESTARTED')
         else:
             pass
+        time.sleep(2)
+
+
+        Pid2 = os.system('pgrep rtorrent')
+        Pid2 = int(Pid2)
+
+        if Pid2 == 256: # no effect of restart so time to repair
+            os.system("app-rtorrent repair")
+            with open(rtorrent_log_file, "a") as f:
+                f.write("\nTIME:"+current_time+"\n")
+                f.write('Restart failed so trying to REPAIR now')
+
+        else:
+            pass
+        time.sleep(2)
+
+        final_pid = os.system('pgrep rtorrent')
+
+        if final_pid == 256: # restart or repair comands doesn't work
+            with open(rtorrent_log_file, "a") as f:
+                f.write("\nTIME:"+current_time+"\n")
+                f.write("\nScript is unable to FIX your rTorrent so please open a support ticket from here - https://my.ultraseedbox.com/submitticket.php\n")
+
+        else:
+            pass
+    
+    
+    
+    
     def docker_app(self):
         #status = os.popen("ps aux | grep -i sonarr").read()
         #print(len(status.splitlines()))
@@ -33,7 +70,10 @@ class app_monitor():
             print("checking status for {}".format(i))
             status = os.popen("ps aux | grep -i {}".format(i)).read()
             count = len(status.splitlines())
-            print("Count of {app} is {count}".format(app=i, count=count))
+            if count <=2:
+                os.system("app-{} upgrade".format(i))
+            else:
+                pass
    
     def create_app_list(self):
         os.system("mkdir {}/bin/app_monitor".format(work_dir))
