9022eda0685a2e6f61188ecf765d4b41d00a8ef4
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-05-11 16:42:28 +0530

    4th commit

--- App_status_check.py
+++ App_status_check.py
@@ -1,12 +1,16 @@
 import os
 import time
 from datetime import datetime
-
+"""
+Data and time for logs to store time app restart time
+"""
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-
+"""
+Variable for location of file below
+"""
 work_dir = os.getcwd()
-
+apps_file= '{}/bin/app_monitor/apps.txt'.format(work_dir)
 class app_monitor():
     def rtorrent_monitor(self):
         status = os.popen("pgrep rtorrent").read()
@@ -22,15 +26,22 @@ class app_monitor():
         this is the approach
         """
     def create_app_list(self):
+        os.system("mkdir {}/bin/app_monitor")
         app_list = list(input("Please enter all applications you want to monitor with a single space in between(for example sonarr radarr lidarr):").split())
         app_list = str(app_list)
-        with open('{}/bin/apps.txt'.format(work_dir),'+w') as f:
+        with open(apps_file,'+w') as f:
             f.write(app_list)
             f.close()
+            
+    
 
 
 
 
 monitor = app_monitor()
 if __name__ == '__main__':
-    monitor.create_app_list()
+    check = os.path.exists(apps_file)
+    if check:
+        monitor.create_app_list()
+    else:
+        pass
--- testing_script.py
+++ testing_script.py
@@ -1,7 +1,3 @@
-str(apps) = list(input("please enter all apps with ").split())
-print("these apps need check",apps)
-apps = str(apps)
-
-with open('appllist.txt', '+w') as f:
-    f.write(apps)
-    f.close()
+import os 
+s = os.path.exists('appllist.txt')
+print(s)
