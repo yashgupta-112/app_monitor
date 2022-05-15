0402b488348ad9ab5c13be23dbb8df48b3675b2f
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-05-15 14:52:34 +0530

    Lord-Uchiha-Madra

--- Final_Monitor_installer.sh
+++ Final_Monitor_installer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 printf "\033[0;31mDisclaimer: This Script is unofficial and Ultra.cc staff will not support any issues with it will monitor your applications and restart them if ap$
 read -rp "Type confirm if you wish to continue: " input
 if [ ! "$input" = "confirm" ]; then
@@ -7,8 +6,8 @@ if [ ! "$input" = "confirm" ]; then
 fi
 
 printf "Please choose option from below if you want notification on discord or as a long file on your service\n"
-printf "1. Store Applications status on your service at {~/scripts/app_monitor}\n"
-printf "2. To get application status on your Discord use your DiscordWebhook URL\n"
+printf "1. Store Applications status on your service at {~/script/app_monitor}\n"
+printf "2. To get application status on your Discord(You need Discord Webhook for it)\n"
 
 read -rp "Please select option 1 or 2: " choice
 
